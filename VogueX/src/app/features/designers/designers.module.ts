00e59fded4fc44b86d6df19fa15e9c4b0948da0d
Darthull lol <phancer666@gmail.com>
2025-06-11 20:25:34 +0200

    Phase 2: Organize all components into feature/components structure

---
+++ VogueX/src/app/features/designers/designers.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DesignersRoutingModule } from './designers-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    DesignersRoutingModule
+  ]
+})
+export class DesignersModule { }
